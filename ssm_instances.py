a14f5f04eed05ee171a708e2ef9bf29485dc6992
elpy1 <elpy@opsdevops.dev>
2020-02-06 01:03:57 +0000

    Simplify ssm script

--- ssm_instances.py
+++ ssm_instances.py
@@ -2,55 +2,56 @@
 import boto3
 import sys
 
+#############################################################
+## Usage:
+## AWS_PROFILE=awscli-profile python3 /path/to/ssm_instances.py
+## or, to update the SSM agent on all instances use:
+## AWS_PROFILE=awscli-profile python3 /path/to/ssm_instances.py update
+#############################################################
+
 def get_ec2_name(iid):
-    ec2i = ec2.Instance(iid)
-    iname = ''
-    for tag in ec2i.tags:
-        if tag['Key'] == 'Name':
-            iname = tag['Value']
-    return iname
+    try:
+        etags = ec2.Instance(iid).tags
+        name = [tag['Value'] for tag in etags if tag['Key'] == 'Name']
+        return name[0] if name else ''
+    except Exception as e:
+        print(f"ERROR: {e}")
 
 def ssm_update_agent(iidlist):
-    resp = ssm.send_command(
-        InstanceIds=iidlist,
-        DocumentName='AWS-UpdateSSMAgent',
-        DocumentVersion='$LATEST')
-    if resp['ResponseMetadata']['HTTPStatusCode'] == 200:
-        print('success')
+    try:
+        resp = ssm.send_command(
+            InstanceIds=iidlist,
+            DocumentName='AWS-UpdateSSMAgent',
+            DocumentVersion='$LATEST')
+        if resp['ResponseMetadata']['HTTPStatusCode'] == 200:
+            print('success')
+    except Exception as e:
+        print(f"ERROR: {e}")
 
 def ssm_list_instances():
     ssmi = ssm.describe_instance_information()['InstanceInformationList']
-    names = []
-    instances = []
-    ips = []
-    updates = []
-    versions = []
-    for x in ssmi:
-        names.append(get_ec2_name(x['InstanceId']))
-        instances.append(x['InstanceId'])
-        ips.append(x['IPAddress'])
-        updates.append(x['IsLatestVersion'])
-        versions.append(x['PlatformName'])
+    names = [get_ec2_name(x['InstanceId']) for x in ssmi]
+    instances = [x['InstanceId'] for x in ssmi]
+    ips = [x['IPAddress'] for x in ssmi]
+    updates = [x['IsLatestVersion'] for x in ssmi]
+    versions = [x['PlatformName'] for x in ssmi]
     titles = ['instance id', 'ip', 'agent up-to-date', 'platform', 'name']
     data = [titles] + list(zip(instances, ips, updates, versions, names))
     return data
 
 ec2 = boto3.resource('ec2')
 ssm = boto3.client('ssm')
-
 data = ssm_list_instances()
 
-if not sys.argv[1:]:
+if (sys.argv[1:] and not 'update' in sys.argv[1].lower()):
+    print(f"  Usage: {sys.argv[0]} or {sys.argv[0]} update")
+    sys.exit()
+
+if sys.argv[1:]:
+    iidlist = [x[0] for x in data[1:]]
+    ssm_update_agent(iidlist)
+else:
     for i, d in enumerate(data):
         line = '|'.join(str(x).ljust(22) for x in d)
         print(line)
-        if i == 0:
-            print('-' * len(line))
-elif sys.argv[1].lower() == 'update':
-    iidlist = []
-    for x in data[1:]:
-        iidlist.append(x[0])
-    ssm_update_agent(iidlist)
-else:
-    print('Unknown argument')
-    sys.exit()
+        if i == 0: print('-' * len(line))
