7c6a50d5c75bc54a8b8d6201e23fc68ccb45ae10
L1 <elpy@opsdevops.dev>
2020-05-11 11:37:09 +1000

    Simplify script
    Convert script into functions. Don't check for AWS credentials or
    session-manager-plugin as these will be checked by the aws command.

--- ssh-ssm.sh
+++ ssh-ssm.sh
@@ -1,54 +1,49 @@
 #!/usr/bin/env bash
-set -o nounset -o pipefail
+set -o nounset -o pipefail -o errexit
 
-if ! type session-manager-plugin &>/dev/null; then
-cat <<EOF && exit 1
-  Error! Unable to find session-manager-plugin. See:
-  https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html
-EOF
-fi
+function main {
+  local ssh_pubkey ssm_cmd
+  local ssh_authkeys='.ssh/authorized_keys'
+  ssh_dir=~/.ssh
 
-[[ "$#" -ne 2 ]] && printf "  Usage: ${0} <instance-id> <ssh user>\n" && exit 1
-if ! grep -q ^AWS_[PS] <(env); then
-  printf "  AWS credentials not found in environment!\n" && exit 1
-fi
-
-if ! [[ "$1" =~ ^i-([0-9a-f]{8,})$ ]]; then
-  printf "  ERROR: invalid instance-id!\n"
-  exit 1
-fi
-
-if [[ "$(ps -o comm= -p $PPID)" != "ssh" ]]; then
-  ssh -o IdentityFile="~/.ssh/ssm-ssh-tmp" -o ProxyCommand="${0} ${1} ${2}" ${2}@${1}
-  exit 0
-fi
-
-function cleanup {
-  rm -f "${ssh_local}"/ssm-ssh-tmp{,.pub}
-}
-
-function tempkey {
-  set -o errexit
-  trap cleanup EXIT
-  ssh-keygen -t ed25519 -N '' -f "${ssh_local}"/ssm-ssh-tmp -C ssm-ssh-session
-  ssh_pubkey=$(< "${ssh_local}"/ssm-ssh-tmp.pub)
-}
-
-ssh_user="$2"
-ssh_authkeys='.ssh/authorized_keys'
-ssh_local=~/.ssh
-ssh_pubkey=$(ssh-add -L 2>/dev/null| head -1) || tempkey
-
-aws ssm send-command \
-  --instance-ids "${1}" \
-  --document-name 'AWS-RunShellScript' \
-  --parameters commands="\"
-    u=\$(getent passwd ${ssh_user}) && x=\$(echo \$u |cut -d: -f6) || exit 1
-    install -d -m700 -o${ssh_user} \${x}/.ssh
-    grep '${ssh_pubkey}' \${x}/${ssh_authkeys} && exit 1
+  checks "$@" && ssh_pubkey=$(ssh-add -L 2>/dev/null| head -1) || mktmpkey
+  ssm_cmd=$(cat <<EOF
+    "u=\$(getent passwd ${2}) && x=\$(echo \$u |cut -d: -f6) || exit 1
+    install -d -m700 -o${2} \${x}/.ssh; grep '${ssh_pubkey}' \${x}/${ssh_authkeys} && exit 1
     printf '${ssh_pubkey}'|tee -a \${x}/${ssh_authkeys} && sleep 15
-    sed -i s,'${ssh_pubkey}',, \${x}/${ssh_authkeys}
-    \"" \
-  --comment "temporary ssm ssh access" #--debug
+    sed -i s,'${ssh_pubkey}',, \${x}/${ssh_authkeys}"
+EOF
+  )
 
-aws ssm start-session --document-name AWS-StartSSHSession --target "${1}" #--debug
+  # put our public key on the remote server
+  aws ssm send-command \
+    --instance-ids "$1" \
+    --document-name "AWS-RunShellScript" \
+    --parameters commands="${ssm_cmd}" \
+    --comment "temporary ssm ssh access" #--debug
+
+  # start ssh session over ssm
+  aws ssm start-session --document-name AWS-StartSSHSession --target "$1" #--debug
+}
+
+function checks {
+  [[ $# -ne 2 ]] && die "Usage: ${0##*/} <instance-id> <ssh user>"
+  [[ ! $1 =~ ^i-([0-9a-f]{8,})$ ]] && die "ERROR: invalid instance-id"
+  if [[ $(ps -o comm= -p $PPID) != "ssh" ]]; then
+    ssh -o IdentityFile="~/.ssh/ssm-ssh-tmp" -o ProxyCommand="${0} ${1} ${2}" "${2}@${1}"
+    exit 0
+  fi
+  pr="$(grep -sl --exclude='*tool-env' "$1" "${ssh_dir}"/ssmtool-*)" &&
+  export AWS_PROFILE=${AWS_PROFILE:-${pr##*ssmtool-}}
+}
+
+function mktmpkey {
+  trap cleanup EXIT
+  ssh-keygen -t ed25519 -N '' -f "${ssh_dir}"/ssm-ssh-tmp -C ssm-ssh-session
+  ssh_pubkey="$(< "${ssh_dir}"/ssm-ssh-tmp.pub)"
+}
+
+function cleanup { rm -f "${ssh_dir}"/ssm-ssh-tmp{,.pub}; }
+function die { echo "[${0##*/}] $*" >&2; exit 1; }
+
+main "$@"
