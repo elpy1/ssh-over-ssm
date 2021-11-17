518b5b8c17a5b7074215552c4bc8c99cab0fee78
L1 <elpy@opsdevops.dev>
2021-11-17 18:52:52 +1100

    misc improvements
    * update exit code for ssm command when ssh pub key already exists on remote
    * better logic if no key found in local ssh agent
    * send errors to /dev/tty to make visible from ssh
    * don't wait for key removal on remote server for ssm send-command
    * use aws waiter and verify ssm command before starting ssh session

--- ssh-ssm.sh
+++ ssh-ssm.sh
@@ -1,52 +1,61 @@
 #!/usr/bin/env bash
 set -o nounset -o pipefail -o errexit
 
+SSH_DIR=$HOME/.ssh
+
 main () {
   local ssh_pubkey ssm_cmd ssh_authkeys='.ssh/authorized_keys'
-  ssh_dir=$HOME/.ssh
 
-  checks "$@" && ssh_pubkey=$(ssh-add -L 2>/dev/null| head -1) || mktmpkey
+  if ! ssh_pubkey=$(ssh-add -L 2>/dev/null| head -1); then
+    if [ ! -f ${SSH_DIR}/ssm-ssh-tmp.pub ]; then
+      ssh-keygen -t ed25519 -N '' -f ${SSH_DIR}/ssm-ssh-tmp -C ssh-over-ssm
+    fi
+    trap cleanup EXIT
+    ssh_pubkey="$(cat ${SSH_DIR}/ssm-ssh-tmp.pub)"
+  fi
+
   ssm_cmd=$(cat <<EOF
     "u=\$(getent passwd ${2}) && x=\$(echo \$u |cut -d: -f6) || exit 1
-    install -d -m700 -o${2} \${x}/.ssh; grep '${ssh_pubkey}' \${x}/${ssh_authkeys} && exit 1
+    [ ! -d \${x}/.ssh ] && install -d -m700 -o${2} \${x}/.ssh
+    grep '${ssh_pubkey}' \${x}/${ssh_authkeys} && exit 0
     printf '${ssh_pubkey}'|tee -a \${x}/${ssh_authkeys} || exit 1
-    (sleep 15 && sed -i s,'${ssh_pubkey}',, \${x}/${ssh_authkeys}) &"
+    (sleep 15 && sed -i s,'${ssh_pubkey}',, \${x}/${ssh_authkeys} &) &>/dev/null"
 EOF
   )
 
   # put our public key on the remote server
-  aws ssm send-command \
+  command_id=$(aws ssm send-command \
     --instance-ids "$1" \
     --document-name "AWS-RunShellScript" \
     --parameters commands="${ssm_cmd}" \
-    --comment "temporary ssm ssh access" #--debug
+    --comment "temporary ssm ssh access" \
+    --output text \
+    --query Command.CommandId)
+
+  # wait for successful send-command execution
+  aws ssm wait command-executed --instance-id "$1" --command-id "${command_id}"
 
   # start ssh session over ssm
-  aws ssm start-session --document-name AWS-StartSSHSession --target "$1" #--debug
+  aws ssm start-session --document-name AWS-StartSSHSession --target "$1"
 }
 
 checks () {
   if [[ $# -ne 2 ]]; then
-    die "Usage: ${0##*/} <instance-id> <ssh user>"
+    die "usage: ${0##*/} <instance-id> <ssh user>"
   elif [[ ! $1 =~ ^i-([0-9a-f]{8,})$ ]]; then
-    die "ERROR: invalid instance-id"
+    die "error: invalid instance-id"
   elif [[ $(basename -- $(ps -o comm= -p $PPID)) != "ssh" ]]; then
     ssh -o IdentityFile="~/.ssh/ssm-ssh-tmp" -o ProxyCommand="${0} ${1} ${2}" "${2}@${1}"
     exit 0
   fi
 
-  if pr="$(grep -sl --exclude='*tool-env' "$1" ${ssh_dir}/ssmtool-*)"; then
+  if pr="$(grep -sl --exclude='*tool-env' "$1" ${SSH_DIR}/ssmtool-*)"; then
     export AWS_PROFILE=${AWS_PROFILE:-${pr##*ssmtool-}}
   fi
 }
 
-mktmpkey () {
-  trap cleanup EXIT
-  ssh-keygen -t ed25519 -N '' -f "${ssh_dir}"/ssm-ssh-tmp -C ssm-ssh-session
-  ssh_pubkey="$(< "${ssh_dir}"/ssm-ssh-tmp.pub)"
-}
+cleanup () { rm -f ${SSH_DIR}/ssm-ssh-tmp{,.pub}; }
+die () { echo "[${0##*/}] $*" >&2 >/dev/tty; exit 1; }
 
-cleanup () { rm -f "${ssh_dir}"/ssm-ssh-tmp{,.pub}; }
-die () { echo "[${0##*/}] $*" >&2; exit 1; }
-
+checks "$@"
 main "$@"
